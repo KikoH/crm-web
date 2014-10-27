a83a7b85c24e07180462afda27d30b8e1b4a936f
Kiko <mmhabib11@gmail.com>
2014-10-27 15:13:53 -0400

    Seperated html document and connected them together

--- server.rb
+++ server.rb
@@ -6,6 +6,9 @@ port = 2000
 server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
 puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
 
+filename = "index.html"
+response = File.read(filename)
+
 loop do                                             # Server runs forever
   client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
 
@@ -14,7 +17,10 @@ loop do                                             # Server runs forever
     lines << line
   end
   puts lines                                        # Output the full request to stdout
+	
 
-  client.puts(Time.now.ctime)                       # Output the current time to the client
+
+  client.puts(response)
+                        # Output the current time to the client
   client.close                                      # Disconnect from the client
 end
