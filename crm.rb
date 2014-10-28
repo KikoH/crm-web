b4af767e96b91b295545d13977c9cd3d1de6f83f
Kiko <mmhabib11@gmail.com>
2014-10-28 15:15:17 -0400

    Added the rolodex class

--- crm.rb
+++ crm.rb
@@ -1,4 +1,5 @@
-require_relative './contact.rb'
+require './contact.rb'
+require './rolodex'
 require 'sinatra'
 
 
@@ -8,10 +9,6 @@ get '/' do
 end
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-	@contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 	erb :contacts
 end
 
