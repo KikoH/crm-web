323c72adad3916a84b56c05934714f61500138fd
Kiko <mmhabib11@gmail.com>
2014-10-28 15:03:23 -0400

    Added the contact.rb class and fake data to show contacts

--- crm.rb
+++ crm.rb
@@ -1,5 +1,6 @@
+require_relative './contact.rb'
 require 'sinatra'
-require 'contact'
+
 
 get '/' do
 	@crm_app_name = "My CRM"
@@ -7,6 +8,10 @@ get '/' do
 end
 
 get '/contacts' do
+	@contacts = []
+	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+	@contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
 	erb :contacts
 end
 
