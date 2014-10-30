8942c4572c17f0f347879d886cddc864032a203b
Kiko <mmhabib11@gmail.com>
2014-10-30 14:59:59 -0400

    Added show_contact.erb and it is funcional

--- crm.rb
+++ crm.rb
@@ -3,6 +3,7 @@ require './rolodex'
 require 'sinatra'
 
 $rolodex = Rolodex.new
+$rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
 
 get '/' do
 	# @crm_app_name = "My CRM"
@@ -15,6 +16,11 @@ end
 
 get '/contacts/new' do
 	erb :new_contact
+end
+
+get '/contacts/1' do
+	@contact = $rolodex.find(1)
+	erb :show_contact
 end
 
 post '/contacts' do
--- rolodex.rb
+++ rolodex.rb
@@ -3,13 +3,16 @@ class Rolodex
 
 	def initialize
 		@contacts = []
-		@id = 0
+		@id = 1
 	end
 
 	def add_contact(contact)
 		contact.id = @id
 		@contacts << contact
 		@id += 1
-		
 	end
+
+	def find(contact_id)
+    	@contacts.find {|contact| contact.id == contact_id }
+  	end
 end
