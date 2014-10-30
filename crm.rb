9e879ae6b63ca0ae36ca1620785dc568d69b5e74
Kiko <mmhabib11@gmail.com>
2014-10-30 15:12:25 -0400

    Added the edit contact class

--- crm.rb
+++ crm.rb
@@ -27,6 +27,15 @@ get '/contacts/:id' do
 	end
 end
 
+get '/contacts/:id/edit'
+	@contact = $rolodex.find(params[:id].to_i)
+	if @contact
+		erb :edit_contact
+	else
+		raise Sinatra::NotFound
+	end
+end
+
 post '/contacts' do
 	puts params
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
