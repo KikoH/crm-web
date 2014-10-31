be445e617c3235d3491fef4812bcebb07f744a44
Kiko <mmhabib11@gmail.com>
2014-10-31 17:11:39 -0400

    Working CRM. Can Add, Edit, Delete and view all contacts.

--- crm.rb
+++ crm.rb
@@ -17,8 +17,6 @@ end
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
-$rolodex = Rolodex.new
-
 get '/' do
 	# @crm_app_name = "My CRM"
   	erb :index
@@ -43,7 +41,7 @@ get '/contacts/:id' do
 end
 
 get '/contacts/:id/edit' do
-	@contact = $rolodex.find(params[:id].to_i)
+	@contact = Contact.get(params[:id].to_i)
 	if @contact
 		erb :edit_contact
 	else
@@ -52,13 +50,13 @@ get '/contacts/:id/edit' do
 end
 
 put "/contacts/:id" do
-  @contact = $rolodex.find(params[:id].to_i)
-  if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.note = params[:note]
-
+	@contact = Contact.get(params[:id].to_i)
+  if @contact.update(
+	:first_name => params[:first_name],
+	:last_name => params[:last_name],
+	:email => params[:email],
+	:note => params[:note]
+	)
     redirect to("/contacts")
   else
     raise Sinatra::NotFound
@@ -66,9 +64,9 @@ put "/contacts/:id" do
 end
 
 delete "/contacts/:id" do
-  @contact = $rolodex.find(params[:id].to_i)
-  if @contact
-    $rolodex.remove_contact(@contact)
+  	@contact = Contact.get(params[:id].to_i)
+  	if @contact
+  		@contact.destroy
     redirect to("/contacts")
   else
     raise Sinatra::NotFound
@@ -76,7 +74,7 @@ delete "/contacts/:id" do
 end
 
 post '/contacts' do
-	contact = Contact.create(
+	@contact = Contact.create(
 	:first_name => params[:first_name],
 	:last_name => params[:last_name],
 	:email => params[:email],
